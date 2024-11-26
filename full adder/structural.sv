3e6a042aef99daf6726d9d39a176ad88dcb05b04
Verma <ananyaverma190@gmail.com>
2024-11-26 22:10:11 +0530

    Update structural.sv

--- full adder/structural.sv
+++ full adder/structural.sv
@@ -2,9 +2,9 @@ module fa(a,b,cin,sum,cout);
 input logic a,b,cin;
 output logic sum,cout;
 wire xor1,and1,and2;
-xor(xor1,a,b);
-xor(sum,xor1,cin);
-and(and1,a,b);
-and(and2,xor1,cin);
-or(cout,and1,and2);
+  xor(xor1,a,b);
+  xor(sum,xor1,cin);
+  and(and1,a,b);
+  and(and2,xor1,cin);
+  or(cout,and1,and2);
 endmodule
